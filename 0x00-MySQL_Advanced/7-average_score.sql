987f7e52bce11f9f42e50ea1b80494d070d943a2
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-25 19:12:08 +0300

    Find average score for student

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,20 @@
--- find average score of each student
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
-DELIMITER $$
-
+-- find the average score for a user 
+DELIMITER //
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    SELECT user_id, project_id, AVG(score) AS average_score
-    FROM corrections
-    WHERE user_id = user_id
-    GROUP BY user_id, project_id;
-END$$
-
+    DECLARE total_score FLOAT;
+    DECLARE total_projects INT;
+    DECLARE avg_score FLOAT;
+    
+    SELECT SUM(score) INTO total_score FROM corrections WHERE user_id = user_id;
+    SELECT COUNT(*) INTO total_projects FROM corrections WHERE user_id = user_id;
+    
+    IF total_projects > 0 THEN
+        SET avg_score = total_score / total_projects;
+    ELSE
+        SET avg_score = 0;
+    END IF;
+    
+    UPDATE users SET average_score = avg_score WHERE id = user_id;
+END //
 DELIMITER ;

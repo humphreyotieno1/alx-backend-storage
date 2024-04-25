719cfd8355e11722d3aab7c2e3ad6a20e951edf7
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-25 18:55:53 +0300

    List band by longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- list bands with Glam as main style
 -- Output band name and style
-SELECT band_name, (2022 - YEAR(formed)) AS lifespan
+SELECT band_name, (IFNULL(split, '2022') - formed) AS lifespan
 FROM metal_bands
-WHERE FIND_IN_SET('Glam rock', split)
+WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
 ORDER BY lifespan DESC;

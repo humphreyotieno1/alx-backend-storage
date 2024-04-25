a70395fe3e31755efb4e0c071549fd3cf7794592
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-25 18:50:18 +0300

    Rank country bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,5 @@
 -- Rank country origins of bands by number of fans
 -- Output country and number of fans
-SELECT origin, SUM(nb_fans) AS nb_fans
-FROM metal_bands
-GROUP BY origin
-ORDER BY nb_fans DESC;
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands
+GROUP BY (origin)
+ORDER BY SUM(fans) DESC;

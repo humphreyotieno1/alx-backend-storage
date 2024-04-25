1a31484841c11dda5bbe69b46506fe3da60f715e
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-04-25 19:49:34 +0300

    Create views

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
--- create views
+-- create views for students who need a meeting
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE score < 80 AND (last_meeting IS NULL OR last_meeting > 1)

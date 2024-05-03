e4f1d521eea0f30239a821bab5a108c0fbdfc2ae
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-04 00:38:20 +0300

    Writing strings to Redis

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,4 @@
+# Redis Basics
+
+## Introduction
+This repository provides a basic introduction to Redis, an open-source in-memory data structure store. Redis is commonly used as a database, cache, and message broker due to its high performance and versatility.
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,17 @@
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        """ Initialize cache """
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store data in redis """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key

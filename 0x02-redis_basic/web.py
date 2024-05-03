cf35b88d7c810f456950ef05e45efd7d86935a11
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-04 01:44:31 +0300

    Expiring web cache and tracker

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,43 +1,34 @@
-import requests
-import time
+#!/usr/bin/env python3
+""" expiring web cache module """
+
 import redis
+import requests
+from typing import Callable
+from functools import wraps
 
-# Connect to Redis
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0)
+redis = redis.Redis()
 
 
-def cache_and_track(func):
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
     def wrapper(url):
-        # Check if URL is cached
-        cached_html = redis_client.get(f"html:{url}")
-        if cached_html:
-            # URL is cached, return cached HTML
-            return cached_html.decode('utf-8')
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
 
-        # URL is not cached, fetch HTML content
-        response = requests.get(url)
-        html_content = response.text
-
-        # Cache HTML content with expiration time of 10 seconds
-        redis_client.setex(f"html:{url}", 10, html_content)
-
-        # Track URL access count
-        redis_client.incr(f"count:{url}")
-
-        return html_content
     return wrapper
 
 
-@cache_and_track
-def get_page(url):
-    return requests.get(url).text
-
-
-if __name__ == "__main__":
-    # Test the get_page function
-    url = "http://slowwly.robertomurray.co.uk/delay/10000/url/http://www.google.com"
-    start_time = time.time()
-    html_content = get_page(url)
-    end_time = time.time()
-    print("HTML content:", html_content)
-    print("Time taken:", end_time - start_time, "seconds")
+@wrap_requests
+def get_page(url: str) -> str:
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text

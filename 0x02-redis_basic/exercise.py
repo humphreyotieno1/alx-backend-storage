3569c7780fc0d47b17eeaf0dcbcc1a4b5e366d3c
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-04 01:07:44 +0300

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,35 +1,40 @@
 #!/usr/bin/env python3
-""" Cache module """
 import redis
 import uuid
 from typing import Union, Callable
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
 
 
 class Cache:
-    """ Cache class """
     def __init__(self):
-        """ Initialize cache """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """ Store data in redis """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
-        """ Get data from redis """
+
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
         value = self._redis.get(key)
         if value is None:
             return None
         if fn is not None:
             value = fn(value)
-            
+        return value
+
     def get_str(self, key: str) -> Union[str, None]:
-        """ Get string from redis """
-        return self.get(key, fn=lambda x: x.decode('utf-8'))
-    
+        return self.get(key, fn=lambda x: x.decode("utf-8"))
+
     def get_int(self, key: str) -> Union[int, None]:
-        """ Get int from redis """
         return self.get(key, fn=int)

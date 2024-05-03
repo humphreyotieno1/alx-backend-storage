7877004bd343161ae83ed5ab90fd97209c751720
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-05-04 01:12:50 +0300

    Incrementing values

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,20 +5,12 @@ from typing import Union, Callable
 from functools import wraps
 
 
-def call_history(method: Callable) -> Callable:
+def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        inputs_key = "{}:inputs".format(method.__qualname__)
-        outputs_key = "{}:outputs".format(method.__qualname__)
-
-        self._redis.rpush(inputs_key, str(args))
-
-        output = method(self, *args, **kwargs)
-
-        self._redis.rpush(outputs_key, output)
-
-        return output
-
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
     return wrapper
 
 
@@ -27,7 +19,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
